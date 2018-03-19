8e780ce41fb9fe3dddbbbbdfac39b68b34ea95c8
georgeperry1 <gmperry18@gmail.com>
2018-03-19 12:35:05 +0100

    feat: SubHeader Component added with basic styles

--- src/components/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const App = () => <div><h1>React Boilerplate</h1></div>;
-
-export default App;
---
+++ src/components/SubHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/index.css';
+import '../styles/SubHeader.css';
+
+const SubHeader = () => (
+  <div className="clearfix subheader-container">
+    <div className="col col-2 subheader-title-container">
+      <p className="h4 subheader-title">Sub header</p>
+    </div>
+  </div>
+);
+
+export default SubHeader;
