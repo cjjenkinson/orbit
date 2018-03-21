6f9e521072bdaa1e40ba93ce066f8bd025ed68bc
Cameron Jenkinson <camjenkinson@gmail.com>
2018-03-21 18:00:50 +0100

    Fix: ensure localState has seamless immutable properties

--- src/helpers/local-storage-helpers.js
+++ src/helpers/local-storage-helpers.js
@@ -1,12 +1,12 @@
 import debounce from 'lodash/debounce';
+import Immutable from 'seamless-immutable';
 
 import { LOCAL_STORAGE_REDUX_KEY } from '../constants';
 
 /**
  * updateLocalStorage
- * When a non-null value is provided, updates local-storage with the supplied
- * value. When `null` is provided, it erases all previously-stored local-storage
- * data
+ * When a non-null value is provided it updates local-storage with the supplied value.
+ * When `null` is provided, it erases all previously-stored local-storage data
  */
 const updateLocalStorage = debounce(
   (value) =>
@@ -17,7 +17,7 @@ const updateLocalStorage = debounce(
 );
 
 export const handleStoreUpdates = (store) => {
-  const relevantState = store.getState();
+  const { ...relevantState } = store.getState();
 
   updateLocalStorage(JSON.stringify(relevantState));
 };
@@ -30,7 +30,8 @@ export const clearReduxState = () => {
 
 export const getInitialState = () => {
   const stateFromStorage = localStorage.getItem(LOCAL_STORAGE_REDUX_KEY);
-  const initialState = JSON.parse(stateFromStorage || '{}');
+
+  const initialState = Immutable(JSON.parse(stateFromStorage || '{}'));
 
   return initialState;
 };
