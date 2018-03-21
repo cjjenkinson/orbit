0326bbd0291757819a20f67c048c566bc4842211
Cameron Jenkinson <camjenkinson@gmail.com>
2018-03-21 09:36:32 +0100

    Ops: add localStorage hydration, dev / prod configuration for store

---
+++ src/helpers/local-storage-helpers.js
@@ -0,0 +1,36 @@
+import debounce from 'lodash/debounce';
+
+import { LOCAL_STORAGE_REDUX_KEY } from '../constants';
+
+/**
+ * updateLocalStorage
+ * When a non-null value is provided, updates local-storage with the supplied
+ * value. When `null` is provided, it erases all previously-stored local-storage
+ * data
+ */
+const updateLocalStorage = debounce(
+  (value) =>
+    value !== null
+      ? localStorage.setItem(LOCAL_STORAGE_REDUX_KEY, value)
+      : localStorage.removeItem(LOCAL_STORAGE_REDUX_KEY),
+  500,
+);
+
+export const handleStoreUpdates = (store) => {
+  const relevantState = store.getState();
+
+  updateLocalStorage(JSON.stringify(relevantState));
+};
+
+export const clearReduxState = () => {
+  window.localStorage.removeItem(LOCAL_STORAGE_REDUX_KEY);
+
+  updateLocalStorage(null);
+};
+
+export const getInitialState = () => {
+  const stateFromStorage = localStorage.getItem(LOCAL_STORAGE_REDUX_KEY);
+  const initialState = JSON.parse(stateFromStorage || '{}');
+
+  return initialState;
+};
