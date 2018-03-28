c700129297d1232f7d1ec5b7cf65ee652aa2099f
Cameron Jenkinson <camjenkinson@gmail.com>
2018-03-28 17:14:35 +0200

    Feat: add isAuthenticated header rendering

--- src/components/LandingView/LandingView.js
+++ src/components/LandingView/LandingView.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const LandingView = () => (
-  <div className="public-landing">
-    <div className="container">
-      <div className="panel">
-        <h2>Orbit - Performance Analytics</h2>
-        <Link to="/login">Login</Link>
-        <Link to="/signup">Sign Up</Link>
-      </div>
-    </div>
-  </div>
-);
+import * as authSelectors from '../../store/Auth/selectors';
 
-export default LandingView;
+class LandingView extends Component {
+  renderUserHeader = () => <Link to="/dashboard">Dashboard</Link>;
+
+  renderAuthHeader = () => (
+    <div>
+      <Link to="/login">Login</Link>
+      <Link to="/signup">Sign Up</Link>
+    </div>
+  );
+  render() {
+    return (
+      <div className="public-landing">
+        <div className="container">
+          <div className="panel">
+            <h2>Orbit - Performance Analytics</h2>
+            {this.props.isAuthenticated ? (
+              this.renderUserHeader()
+            ) : (
+              this.renderAuthHeader()
+            )}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+LandingView.propTypes = {
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => {
+  const isAuthenticated = authSelectors.getAuthStatus(state);
+
+  return {
+    isAuthenticated,
+  };
+};
+
+export default connect(mapStateToProps)(LandingView);
