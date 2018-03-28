0d82932f2901e9e102d628e5da6f2e3ef9eb28b6
Ben Eisenberg <ben.iceberg@gmail.com>
2018-03-28 11:03:02 +0200

    feat: changes list item so it gets props from parent

--- src/components/PerformanceListItem/PerformanceListItem.js
+++ src/components/PerformanceListItem/PerformanceListItem.js
@@ -1,25 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './PerformanceListItem.css';
-
-const PerformanceListItem = ({ performance, enabler }) => {
-  const width = `${performance * 100}%`;
+/* eslint-disable */
+const PerformanceListItem = ({ score, enabler }) => {
+  const width = `${score * 100}%`;
+  const strong = (score > 0.7) ? 'STRONG' : ((score < 0.5) ? 'WEAK' : 'AVERAGE');
   return (
     <div className="PerformanceListItem">
-      <strong>STRONG</strong>
+      <strong>{strong}</strong>
       <div className="performance">
         <p>{enabler}</p>
         <div className="performance-bar">
           <span className="performance-fill" style={{ width }} />
         </div>
-        <div className="performance-value">{performance * 10}</div>
+        <div className="performance-value">{score * 10}</div>
       </div>
     </div>
   );
 };
-
+/* eslint-disable */
 PerformanceListItem.propTypes = {
-  performance: PropTypes.number,
+  score: PropTypes.number,
   enabler: PropTypes.string,
 };
 
