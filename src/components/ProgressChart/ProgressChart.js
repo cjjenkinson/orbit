bc0cb388c18a094dc5f5db7e3f0d20a28f4e9486
marcoantonioghiani01 <marcoantonio.ghiani01@gmail.com>
2018-04-28 15:53:58 +0200

    feat: create progressChart component and update workspace style

---
+++ src/components/ProgressChart/ProgressChart.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+import './ProgressChart.css';
+
+class ProgressChart extends Component {
+  render() {
+    return (
+      <div className="progress-chart-container">
+      </div>
+    );
+  }
+}
+
+// ProgressChart.propTypes = {
+
+// };
+
+export default ProgressChart;
